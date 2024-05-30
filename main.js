cd87304736ce0ec1cbae6171c0a84a85b673606d
Diego Bottegoni <146596468+DiegoBottegoni@users.noreply.github.com>
2024-05-30 11:15:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const { HashTable } = require('./src/models/CustomHashTable.js');
 
 // Inserting, getting and deleting from HashTable
 const hashTable = new HashTable();
-hashTable.insert('name', 'Diego');
-hashTable.insert('age', 36);
+hashTable.set('name', 'Diego');
+hashTable.set('age', 36);
 console.log(hashTable.get('name')); // Output: Diego
 console.log(hashTable.get('age'));  // Output: 36
 hashTable.delete('name');
 console.log(hashTable.get('name')); // Output: undefined
+
+hashTable.set('key', 'value1');
+hashTable.set('key', 'value2');
+console.log(hashTable.get('key'));
